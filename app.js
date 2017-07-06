74474f717088a7157f0026e3f2768b379a12abb1
diop <fodesdiop@gmail.com>
2017-07-06 09:38:43 -0700

    update required packages

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Xamxam Institute of Technology @ Google Cloud 2017')
+  res.send('Xamxam Institute of Technology © 2017')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
