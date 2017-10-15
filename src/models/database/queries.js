a8bf6a3ae6e2cae984edac987c8bd732c80f9760
diop <fodesdiop@gmail.com>
2017-10-14 17:58:34 -0700

    Added some wolof resources

--- src/models/database/queries.js
+++ src/models/database/queries.js
@@ -6,21 +6,25 @@ const queries = {
     return database.any(`SELECT * FROM courses`)
   },
 
-  getCourseByID: (courseID) => {
+  getCourseByID: courseID => {
     return database.any(`SELECT * FROM courses WHERE id = $1`, [courseID])
   },
 
-  createstudent: (name, email, password) => {
+  createStudent: (name, email, password) => {
     return database.one(`INSERT INTO student (name, email, password)
       VALUES ($1, $2, $3) RETURNING *`, [name, email, password])
   },
 
-  getstudentById: (studentID) => {
+  getStudentById: studentID => {
     return database.one('SELECT id, name, email, date_joined FROM student WHERE id = $1', [studentID])
   },
 
-  getstudentByEmail: (email) => {
+  getStudentByEmail: email => {
     return database.one('SELECT * FROM student WHERE email = $1', [email])
+  },
+
+  createReview: (content, album_id, student_id) => {
+    return database.none(`INSERT INTO reviews (content, album_id, student_id) VALUES ($1, $2, $3)`, [content, album_id, student_id])
   },
 
   getThreeNewestReviews: () => {
@@ -32,11 +36,7 @@ const queries = {
       LIMIT 3`)
   },
 
-  createReview: (content, album_id, student_id) => {
-    return database.none(`INSERT INTO reviews (content, album_id, student_id) VALUES ($1, $2, $3)`, [content, album_id, student_id])
-  },
-
-  getReviewsByStudentId: (studentID) => {
+  getReviewsByStudentId: studentID => {
     return database.any(`SELECT reviews.*, courses.title AS album_title, student.name AS student_name
       FROM reviews
       INNER JOIN student ON reviews.student_id = student.id
@@ -45,19 +45,19 @@ const queries = {
       ORDER BY reviews.date_created DESC`, [studentID])
   },
 
-  getReviewsByCourseId: (courseID) => {
+  getReviewsByCourseId: courseID => {
     return database.any(`SELECT * FROM reviews WHERE album_id = $1`, [courseID])
   },
 
-  getReviewsByCourseId: (courseID) => {
-    return database.any(`SELECT reviews.id, reviews.content, reviews.album_id, reviews.student_id, reviews.date_created, courses.title AS album_title, student.name AS student_name FROM reviews
+  getReviewsByCourseId: courseID => {
+    return database.any(`SELECT reviews.*, courses.title AS album_title, student.name AS student_name FROM reviews
       INNER JOIN student ON reviews.student_id = student.id
       INNER JOIN courses ON reviews.album_id = courses.id
       WHERE reviews.album_id = $1
       ORDER BY reviews.date_created DESC`, [courseID])
   },
 
-  deleteByReviewID: (reviewID) => {
+  deleteByReviewID: reviewID => {
     return database.none('DELETE FROM reviews WHERE id = $1', [reviewID]);
   }
 
